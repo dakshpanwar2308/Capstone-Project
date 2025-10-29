73a90a99aeffb664f49913df473d2336b720f138
dakshpanwar2308 <dakshpanwar2308@gmail.com>
2025-10-29 14:53:08 -0600

    Added movie posters

---
+++ backend/movies-service/src/main/resources/db/migration/V4__update_movie_posters.sql
@@ -0,0 +1,7 @@
+UPDATE movies SET poster_url = '/images/galactic-voyage.jpg' WHERE title = 'The Galactic Voyage';
+UPDATE movies SET poster_url = '/images/mystery-manor.jpg' WHERE title = 'Mystery Manor';
+UPDATE movies SET poster_url = '/images/saffron-dreams.jpg' WHERE title = 'Saffron Dreams';
+UPDATE movies SET poster_url = '/images/echoes-of-time.jpg' WHERE title = 'Echoes of Time';
+UPDATE movies SET poster_url = '/images/aurora-nights.jpg' WHERE title = 'Aurora Nights';
+UPDATE movies SET poster_url = '/images/legends-of-the-savannah.jpg' WHERE title = 'Legends of the Savannah';
+UPDATE movies SET poster_url = '/images/rhythm-street.jpg' WHERE title = 'Rhythm Street';
